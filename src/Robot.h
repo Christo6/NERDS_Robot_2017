c8a710122d26f600b2a2f24774c071300645c9cc
Ian Bogardt <dagr8lucario@gmail.com>
2017-02-21 16:18:34 -0700

    Initial commit: Fixed templates.

---
+++ src/Robot.h
@@ -0,0 +1,30 @@
+#ifndef ROBOT_H
+#define ROBOT_H
+
+#include <memory>
+
+#include <Commands/Command.h>
+#include <Commands/Scheduler.h>
+#include <IterativeRobot.h>
+#include <LiveWindow/LiveWindow.h>
+#include <SmartDashboard/SendableChooser.h>
+#include <SmartDashboard/SmartDashboard.h>
+
+class Robot : public frc::IterativeRobot{
+public:
+	void RobotInit() override;
+	void DisabledInit() override;
+	void DisabledPeriodic() override;
+	void AutonomousInit() override;
+	void AutonomousPeriodic() override;
+	void TeleopInit() override;
+	void TeleopPeriodic() override;
+	void TestPeriodic() override;
+
+	//static shared pointers of each subsystem
+private:
+	frc::SendableChooser<frc::Command*> chooser;
+}
+
+
+#endif //ROBOT_H
