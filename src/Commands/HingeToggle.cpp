0afba40b3a00542524b32264a3efa1bde4ecb2f1
Ian Bogardt <dagr8lucario@gmail.com>
2017-02-21 20:35:53 -0700

    Added the HingeToggle command

---
+++ src/Commands/HingeToggle.cpp
@@ -0,0 +1,35 @@
+#include "HingeToggle.h"
+
+HingeToggle::HingeToggle() {
+	Requires(Robot::pneumatics.get());
+}
+
+// Called just before this Command runs the first time
+void HingeToggle::Initialize() {
+	if (Robot::pneumatics->GetHingeState()){
+		Robot::pneumatics->CloseHinge();
+	} else {
+		Robot::pneumatics->OpenHinge();
+	}
+}
+
+// Called repeatedly when this Command is scheduled to run
+void HingeToggle::Execute() {
+
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool HingeToggle::IsFinished() {
+	return true;
+}
+
+// Called once after isFinished returns true
+void HingeToggle::End() {
+
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void HingeToggle::Interrupted() {
+
+}
