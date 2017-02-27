26cbb320ddd54851fbef812d50334cd1b6869d19
Omari Hughes <omegagamer67@gmail.com>
2017-02-27 16:30:27 -0700

    Added Encoder Functions and Gyro Functions

--- src/Subsystems/DriveTrain.cpp
+++ src/Subsystems/DriveTrain.cpp
@@ -4,7 +4,8 @@
 DriveTrain::DriveTrain() : Subsystem("DriveTrain"),
 	drive(std::make_unique<RobotDrive>(DRIVE_LEFT, DRIVE_RIGHT)),
 	encoder_left(std::make_unique<Encoder>(ENCODER_LEFT_A, ENCODER_LEFT_B)),
-	encoder_right(std::make_unique<Encoder>(ENCODER_RIGHT_A, ENCODER_RIGHT_B))
+	encoder_right(std::make_unique<Encoder>(ENCODER_RIGHT_A, ENCODER_RIGHT_B)),
+	gyro(std::make_unique<ADXRS450_Gyro>())
 {
 
 }
@@ -17,4 +18,25 @@ void DriveTrain::InitDefaultCommand() {
 
 void DriveTrain::ArcadeDrive(double speed, double turn){
 	drive->ArcadeDrive(speed, turn);
+}
+
+void DriveTrain::ResetEncoders() {
+	encoder_right->Reset();
+	encoder_left->Reset();
+}
+
+int DriveTrain::GetRightEncoder() {
+	return encoder_right->Get();
+}
+
+int DriveTrain::GetLeftEncoder() {
+	return encoder_left->Get();
+}
+
+void DriveTrain::ResetGyro(){
+	gyro->Reset();
+}
+
+double DriveTrain::GetAngle(){
+	return gyro->GetAngle();
 }
--- src/Subsystems/DriveTrain.h
+++ src/Subsystems/DriveTrain.h
@@ -5,6 +5,7 @@
 #include <Commands/Subsystem.h>
 #include <RobotDrive.h>
 #include <Encoder.h>
+#include <ADXRS450_Gyro.h>
 
 #include "../RobotMap.h"
 
@@ -13,10 +14,16 @@ public:
 	DriveTrain();
 	void InitDefaultCommand();
 	void ArcadeDrive(double, double);
+	void ResetEncoders();
+	int GetRightEncoder();
+	int GetLeftEncoder();
+	void ResetGyro();
+	double GetAngle();
 private:
 	std::unique_ptr<RobotDrive> drive;
 	std::unique_ptr<Encoder> encoder_left;
 	std::unique_ptr<Encoder> encoder_right;
+	std::unique_ptr<ADXRS450_Gyro> gyro;
 };
 
 #endif  // DriveTrain_H
